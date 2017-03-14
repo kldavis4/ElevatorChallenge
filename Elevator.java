bce7119d780f6c941925b3e2ab09921a5a65b4e6
eaustin7024 <eaustin7024@gmail.com>
2017-03-14 17:41:13 -0500

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -1,1 +1,10 @@
-/** * Interface that defines what an Elevator can do */public interface Elevator {  public void goUp();  public void goDown();  public ElevatorDirection getDirection();  public void addNewDestination(Integer destination);  public void removeDestination();  public ElevatorStatus getStatus();  public void ringAlarm();}
+/** * Interface that defines what an Elevator can do */
+public interface Elevator {  
+  public void goUp();  
+  public void goDown();  
+  public ElevatorDirection getDirection();  
+  public void addNewDestination(Integer destination);  
+  public void removeDestination();  
+  public ElevatorStatus getStatus();  
+  public void ringAlarm();
+}
