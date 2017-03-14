f0d5a11d5c63b5bf3e0942c0b870fbbdc53feb1c
eaustin7024 <eaustin7024@gmail.com>
2017-03-14 18:34:06 -0500

    Create ElevatorControlSystem.java

---
+++ ElevatorControlSystem.java
@@ -0,0 +1,1 @@
+/** * Interface for the elevator control system */public interface ElevatorControlSystem {   public void monitor();   public void pickUpPassenger(Integer pickUpFloor);   public void toDestination(Integer elevatorId, Integer destinationFloor);}
