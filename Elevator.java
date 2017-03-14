da44d715ba45885edc30c84c7ee618530c8d6dea
eaustin7024 <eaustin7024@gmail.com>
2017-03-14 17:33:32 -0500

    Create Elevator.java

---
+++ Elevator.java
@@ -0,0 +1,1 @@
+/** * Interface that defines what an Elevator can do */public interface Elevator {  public void goUp();  public void goDown();  public ElevatorDirection getDirection();  public void addNewDestination(Integer destination);  public void removeDestination();  public ElevatorStatus getStatus();  public void ringAlarm();}
