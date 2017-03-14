eaae5b1ad12b728f5cf301a2495e8d79d7288de6
eaustin7024 <eaustin7024@gmail.com>
2017-03-14 17:57:16 -0500

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -1,5 +1,9 @@
 /** * Interface that defines what an Elevator can do */
 public interface Elevator {  
+  
+  public static final int MIN_FLOOR = 1; 
+  public static final int MAX_FLOOR = 20;
+  
   public void goUp();  
   public void goDown();  
   public ElevatorDirection getDirection();  
