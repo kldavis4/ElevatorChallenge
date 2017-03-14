9db0ee86faabb53397831131392e4b34fd74492f
eaustin7024 <eaustin7024@gmail.com>
2017-03-14 17:39:36 -0500

    Update ElevatorState.java

--- ElevatorState.java
+++ ElevatorState.java
@@ -1,1 +1,6 @@
-/** * @author Erica Austin * Enum to hold the elevator state */public enum ElevatorState {  ELEVATOR_IN_SERVICE,  ELEVATOR_MAINTENANCE,}
+/** * @author Erica Austin * Enum to hold the elevator state */
+public enum ElevatorState 
+{  
+  ELEVATOR_IN_SERVICE,  
+  ELEVATOR_MAINTENANCE;
+}
